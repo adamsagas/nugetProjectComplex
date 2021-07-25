01f62ddc6abc782f3f9f830fdc3fe014828516e0
adamshagash <78692714+adamsagas@users.noreply.github.com>
2021-07-25 20:16:38 +0300

    asd

---
+++ Sdk_1/Sdk_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sdk_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
