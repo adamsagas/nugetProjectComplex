0caff29e5b5417b2e1dd7168c1f899a77e46c86a
adamshagash <78692714+adamsagas@users.noreply.github.com>
2021-07-25 18:55:27 +0300

    Add sdj & non-sdk projects

---
+++ Sdk_1/Non-Sdk_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Non_Sdk_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
